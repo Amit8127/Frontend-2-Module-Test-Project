2990dce0c20d5bb88efcbc71dac3baa294bc0e95
Amit8127 <amitmonu0000@gmail.com>
2023-09-04 02:52:43 +0530

    update

--- script.js
+++ script.js
@@ -1,61 +1,99 @@
-const page = document.getElementById("veiwCont");
-const apiKey = "d07e6af81e0b30fa8e2bb3a0fe8ed13d";
-const url = "https://api.openweathermap.org/data/2.5/weather?";
-
-
-function getLocation() {
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        alert("Somthing is wrong");
-    }
-}
-
-function showPosition(position) {
-
-    async function checkWeather() {
-        const responce = await fetch(url + `lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`);
-        var data = await responce.json();
-
-        document.querySelector(".location").innerHTML = "Location : "+data.name;
-        document.querySelector(".wind").innerHTML = "Wind Speed : "+ data.wind.speed+ "kmph";
-        document.querySelector(".humidity").innerHTML ="Humidity : "+data.main.humidity+"%";
-        document.querySelector(".pressure").innerHTML ="Pressure : "+Math.round(data.main.pressure * 0.000987 )  +"atm";
-        document.querySelector(".temp").innerHTML ="Feels like : "+ Math.round(data.main.temp)+"°c";
-    }
+document.addEventListener("DOMContentLoaded", () => {
+    const fetchDataButton = document.getElementById("fetchDataButton");
+    const homePage = document.getElementById("home");
+  
+  
+    function getWindDirection(degrees) {
+      // Define an array of direction names
+      const directions = [
+        "North", "North-Northeast", "Northeast", "East-Northeast",
+        "East", "East-Southeast", "Southeast", "South-Southeast",
+        "South", "South-Southwest", "Southwest", "West-Southwest",
+        "West", "West-Northwest", "Northwest", "North-Northwest"
+      ];
     
-    checkWeather();
-    page.innerHTML = "";
-    page.className ="";
-    const div = document.createElement("div");
-    div.className = "dataPage";
-    div.innerHTML = `
-    <div class="col1">
-        <h1>Welcome To The Weather App</h1>
-        <p>Here is your current location</p>
-        <div class="ll">
-            <p>Lat : ${position.coords.latitude}</p>
-            <p>Long : ${position.coords.longitude}</p>
-        </div>
-    </div>
-    <div class="col2" id="veiw">
-        <iframe src="https://maps.google.com/maps?q=${position.coords.latitude}, ${position.coords.longitude}&output=embed" 
-            width="100%"
-            height="100%"
-        ></iframe>
-    </div>
-    <div class="col3">
-        <h1>Your Weather Data</h1>
-        <div class="col3-data">
-        <p class="location">Location: New Delhi</p>
-        <p class="wind">Wind Speed: 100kmph</p>
-        <p class="humidity">Humidity : 10</p>
-        <p class="time">Time Zone : GMT +5:30</p>
-        <p class="pressure">Pressure: 10atm</p>
-        <p class="windDir">Wind Direction : North West</p>
-        <p class="UV-idx">UV Index : 500</p>
-        <p class="temp">Feels like: 30°</p>
-        </div>
-    </div>`;
-    page.appendChild(div);
-}
+      // Calculate the index in the array based on degrees
+      const index = Math.round(degrees / 22.5);
+      
+      // Ensure the index is within valid bounds (0-15)
+      const normalizedIndex = (index + 16) % 16;
+    
+      // Return the corresponding wind direction
+      console.log(directions[normalizedIndex]);
+      return directions[normalizedIndex];
+    }
+  
+    fetchDataButton.addEventListener("click", () => {
+      // Get user's geolocation
+      if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+  
+          // Display the user's location on Google Maps
+          displayMap(latitude, longitude);
+  
+          // Fetch weather data using OpenWeatherMap API
+          fetchWeatherData(latitude, longitude);
+        });
+      } else {
+        alert("Geolocation is not available in this browser.");
+      }
+    });
+  
+    function displayMap(latitude, longitude) {
+      homePage.innerHTML = "";
+      homePage.id = "";
+      homePage.innerHTML = `<div class="col1">
+          <h1>Welcome To The Weather App</h1>
+          <p>Here is your current location</p>
+          <div class="ll">
+              <p>Lat : ${latitude}</p>
+              <p>Long : ${longitude}</p>
+          </div>
+      </div>
+      <div class="col2" id="veiw">
+          <iframe src="https://maps.google.com/maps?q=${latitude}, ${longitude}&output=embed" 
+              width="100%"
+              height="100%"
+          ></iframe>
+      </div>`;
+    }
+  
+    function fetchWeatherData(latitude, longitude) {
+      // Replace 'YOUR_OPENWEATHERMAP_API_KEY' with your actual API key
+      const apiKey = "d07e6af81e0b30fa8e2bb3a0fe8ed13d";
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly,daily&units=metric&appid=${apiKey}`;
+  
+      fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+          // Handle and display weather data
+          displayWeatherData(data);
+        })
+        .catch((error) => {
+          console.error("Error fetching weather data:", error);
+        });
+    }
+  
+    function displayWeatherData(data) {
+      // Extract and display relevant weather information
+      console.log(data);
+      const weatherHTML = document.createElement("div");
+      weatherHTML.className = "col3";
+      weatherHTML.innerHTML = `
+          <h1>Your Weather Data</h1>
+          <div class="col3-data">
+          <p class="location">Location : ${data.name}</p>
+          <p class="wind">Wind Speed : ${Math.round(data.wind.speed*3.6)}kmph</p>
+          <p class="humidity">Humidity : ${data.main.humidity}%</p>
+          <p class="time">Time Zone : GMT +5:30</p>
+          <p class="pressure">Pressure: ${Math.round(data.main.pressure * 0.000987)}atm</p>
+          <p class="windDir">Wind Direction : ${getWindDirection(data.wind.deg)}</p>
+          <p class="UV-idx">UV Index : 500</p>
+          <p class="temp">Feels like : ${Math.round(data.main.feels_like)}°c</p>
+          `;
+  
+      homePage.appendChild(weatherHTML);
+    }
+  });
